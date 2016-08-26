026bee4507566d9e70c142a32a5020b48242bee1
Dmitry Fil <monkdaf@gmail.com>
2016-08-26 12:58:20 +0300

    Add script for insert data to insISA95_EquipmentClass

---
+++ insISA95_EquipmentClass.sql
@@ -0,0 +1,17 @@
+﻿--- use DB as default one
+USE [KRR-PA-ISA95_PRODUCTION]
+GO
+
+--- DELETE FROM [dbo].[EquipmentClass] WHERE ID >= 10000
+--- Insert data to table to Equipment
+INSERT INTO [dbo].[EquipmentClass](Description, Location, EquipmentLevel, HierarchyScope, ParentID, ID, Code) VALUES (N'Точки измерения', NULL, NULL, NULL, NULL, 10000, NULL);
+INSERT INTO [dbo].[EquipmentClass](Description, Location, EquipmentLevel, HierarchyScope, ParentID, ID, Code) VALUES (N'Точки измерения температуры', NULL, NULL, NULL, 10000, 10001, NULL);
+INSERT INTO [dbo].[EquipmentClass](Description, Location, EquipmentLevel, HierarchyScope, ParentID, ID, Code) VALUES (N'Точки измерения давления', NULL, NULL, NULL, 10000, 10002, NULL);
+INSERT INTO [dbo].[EquipmentClass](Description, Location, EquipmentLevel, HierarchyScope, ParentID, ID, Code) VALUES (N'Точки измерения расхода', NULL, NULL, NULL, 10000, 10003, NULL);
+INSERT INTO [dbo].[EquipmentClass](Description, Location, EquipmentLevel, HierarchyScope, ParentID, ID, Code) VALUES (N'Точки измерения концентрации', NULL, NULL, NULL, 10000, 10004, NULL);
+INSERT INTO [dbo].[EquipmentClass](Description, Location, EquipmentLevel, HierarchyScope, ParentID, ID, Code) VALUES (N'Точки измерения нормализованного расхода', NULL, NULL, NULL, 10000, 10005, NULL);
+INSERT INTO [dbo].[EquipmentClass](Description, Location, EquipmentLevel, HierarchyScope, ParentID, ID, Code) VALUES (N'Точки измерения приведенного расхода', NULL, NULL, NULL, 10000, 10006, NULL);
+INSERT INTO [dbo].[EquipmentClass](Description, Location, EquipmentLevel, HierarchyScope, ParentID, ID, Code) VALUES (N'Шкафы учёта', NULL, NULL, NULL, NULL, 10007, NULL);
+
+GO
+
