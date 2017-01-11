4a240366e5e2413d208f424c140dc93314fb7e53
monkdaf <monkdaf@gmail.com>
2017-01-11 10:39:18 +0200

    Add stored procedure fo transfer all current data (from service)

---
+++ StoredProcedure/create_gcCurrTransfer.sql
@@ -0,0 +1,26 @@
+﻿USE [KRR-PA-CNT-GasForISA95]
+GO
+/****** Object:  StoredProcedure [dbo].[gcCurrTransfer]    Script Date: 1/11/2017 10:33:02 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[gcCurrTransfer]
+
+AS
+BEGIN
+
+	SET NOCOUNT ON;
+	EXEC gcCurDateTransfer01
+	EXEC gcCurDateTransfer02
+	EXEC gcCurDateTransfer03
+	EXEC gcCurDateTransfer04
+	EXEC gcCurDateTransfer05
+	EXEC gcCurDateTransfer07
+--	EXEC gcCurDateTransfer08
+--	EXEC gcCurDateTransfer09
+	EXEC gcCurDateTransfer10
+--	EXEC gcCurDateTransfer11
+
+END
