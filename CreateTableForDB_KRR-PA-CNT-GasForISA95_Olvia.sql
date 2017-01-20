df26542bb9f27b8febfb1bd9be6185f058ad78e2
monkdaf <monkdaf@gmail.com>
2017-01-20 12:22:43 +0200

    add table for Olvia in our DB

---
+++ CreateTableForDB_KRR-PA-CNT-GasForISA95_Olvia.sql
@@ -0,0 +1,36 @@
+﻿--- use DB as default one
+USE [KRR-PA-CNT-GasForISA95]
+GO
+
+--- create table for recive data from PLC
+CREATE TABLE [dbo].[PlcCurOlvia](
+	[dt] [datetime] NOT NULL,
+	[PE4a2] [real] NOT NULL,
+	[TE1a2] [real] NOT NULL,
+	[QE1a2] [real] NOT NULL,
+	[FE1a2_norm] [real] NOT NULL,
+	[FE1a2_norm_Q] [real] NOT NULL,
+	[PE4a1] [real] NOT NULL,
+	[TE1a1] [real] NOT NULL,
+	[QE1a1] [real] NOT NULL,
+	[FE1a1_norm] [real] NOT NULL,
+	[FE1a1_norm_Q] [real] NOT NULL
+) ON [PRIMARY]
+
+GO
+
+CREATE TABLE [dbo].[PlcSumOlvia](
+	[dt] [datetime] NOT NULL,
+	[PE4a2] [real] NOT NULL,
+	[TE1a2] [real] NOT NULL,
+	[QE1a2] [real] NOT NULL,
+	[FE1a2_norm] [real] NOT NULL,
+	[FE1a2_norm_Q] [real] NOT NULL,
+	[PE4a1] [real] NOT NULL,
+	[TE1a1] [real] NOT NULL,
+	[QE1a1] [real] NOT NULL,
+	[FE1a1_norm] [real] NOT NULL,
+	[FE1a1_norm_Q] [real] NOT NULL
+) ON [PRIMARY]
+
+GO
