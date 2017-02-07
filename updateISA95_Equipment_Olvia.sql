1e3a02b5fdd7e60cc8d4b0325ee13f66ead0398e
monkdaf <monkdaf@gmail.com>
2017-02-07 14:08:56 +0200

    change name equipment for Olvia

---
+++ updateISA95_Equipment_Olvia.sql
@@ -0,0 +1,15 @@
+UPDATE [KRR-PA-ISA95_PRODUCTION].[dbo].[Equipment]
+	SET Description = N'КРП-2(HES), кислород на входе в коллектор, л.1'
+	WHERE ID = 10210; 
+
+UPDATE [KRR-PA-ISA95_PRODUCTION].[dbo].[Equipment]
+	SET Description = N'КРП-2(HES), кислород на входе в коллектор, л.2'
+	WHERE ID = 10211; 
+
+UPDATE [KRR-PA-ISA95_PRODUCTION].[dbo].[Equipment]
+	SET Description = N'КРП-2(HES), давл №1 кислорода на вых из коллектора'
+	WHERE ID = 10212; 
+
+UPDATE [KRR-PA-ISA95_PRODUCTION].[dbo].[Equipment]
+	SET Description = N'КРП-2(HES), давл №2 кислорода на вых из коллектора'
+	WHERE ID = 10213; 
